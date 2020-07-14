610a8da9939cac2b6c0dc66ec96b87beb62a90ff
KuzMenachem <35711942+KuzMenachem@users.noreply.github.com>
2020-07-14 14:48:27 +0200

    Update fixed_grid.py

--- tfdiffeq/fixed_grid.py
+++ tfdiffeq/fixed_grid.py
@@ -15,7 +15,9 @@ class Euler(FixedGridODESolver):
 class Midpoint(FixedGridODESolver):
 
     def step_func(self, func, t, dt, y):
-        return tuple(dt * f_ for f_ in cast_double(func(t + dt / 2, y_mid)))
+        y_mid = tuple(y_ + f_ * dt / 2 for y_, f_ in zip(y, func(t, y)))
+        return tuple(dt * f_ for f_ in func(t + dt / 2, y_mid))
+
 
     @property
     def order(self):
